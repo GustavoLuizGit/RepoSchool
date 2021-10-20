b71a509c8c58f5e7319c468f03987541cbf18820
GustavoLuizGit <glogustavo4321@gmail.com>
2021-10-20 20:36:42 -0300

    FormsAlterar-DadosDoSqlParaOComboBox
    Conectei o metodo do forms como o Sql e fiz o combo box e o textbox receber os dados do SQL

--- WindonsForm-LTPII/ProjectAgenda-SQLSERVER/ProjetoAgenda/ProjetoAgenda/FrmAlterar.Designer.cs
+++ WindonsForm-LTPII/ProjectAgenda-SQLSERVER/ProjetoAgenda/ProjetoAgenda/FrmAlterar.Designer.cs
@@ -32,8 +32,8 @@ namespace ProjetoAgenda
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.txtCidade = new System.Windows.Forms.TextBox();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.cmdAlterar = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
@@ -72,32 +72,34 @@ namespace ProjetoAgenda
             this.label3.TabIndex = 2;
             this.label3.Text = "Cidade ";
             // 
-            // textBox2
+            // txtTelefone
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(161, 185);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(242, 28);
-            this.textBox2.TabIndex = 4;
+            this.txtTelefone.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTelefone.Location = new System.Drawing.Point(161, 185);
+            this.txtTelefone.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(242, 28);
+            this.txtTelefone.TabIndex = 4;
             // 
-            // textBox3
+            // txtCidade
             // 
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(161, 244);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(4);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(242, 28);
-            this.textBox3.TabIndex = 5;
+            this.txtCidade.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCidade.Location = new System.Drawing.Point(161, 244);
+            this.txtCidade.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(242, 28);
+            this.txtCidade.TabIndex = 5;
             // 
             // comboBox1
             // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
             this.comboBox1.Location = new System.Drawing.Point(161, 124);
             this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(242, 30);
             this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // cmdAlterar
             // 
@@ -128,8 +130,8 @@ namespace ProjetoAgenda
             this.Controls.Add(this.label4);
             this.Controls.Add(this.cmdAlterar);
             this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtCidade);
+            this.Controls.Add(this.txtTelefone);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -137,7 +139,7 @@ namespace ProjetoAgenda
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "FrmAlterar";
             this.Text = "FrmAlterar";
-        
+            this.Load += new System.EventHandler(this.FrmAlterar_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -148,8 +150,8 @@ namespace ProjetoAgenda
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.TextBox txtCidade;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button cmdAlterar;
         private System.Windows.Forms.Label label4;
--- WindonsForm-LTPII/ProjectAgenda-SQLSERVER/ProjetoAgenda/ProjetoAgenda/FrmAlterar.cs
+++ WindonsForm-LTPII/ProjectAgenda-SQLSERVER/ProjetoAgenda/ProjetoAgenda/FrmAlterar.cs
@@ -18,24 +18,59 @@ namespace ProjetoAgenda
         {
             InitializeComponent();
 
+        {
+            SqlConnection cn = new SqlConnection();
             cn.ConnectionString = "SERVER = LAPTOP-PG3TNAT4\\SQLEXPRESS;Database=Agenda;UID=sa;PWD=123;";
             cn.Open();
 
-            SqlDataAdapter sda = new SqlDataAdapter("Select Nome from DadosAgenda", cn);
-            DataTable dt = new DataTable();
-            sda.Fill(dt);
-            foreach (DataRow row in dt.Rows)
-            {
-                comboBox1.Items.Add(row["Nome"].ToString());
-            }
+            SqlCommand cd = new SqlCommand();
+            cd.Connection = cn;
+
+            cd.CommandText = "Update DadosAgenda set Telefone = '" + txtTelefone.Text + "',Cidade = '" + txtCidade.Text + "'Where Codigo =" + codigoAgenda.ToString();
+
+            MessageBox.Show("Registro alterado com sucesso!");
+
+            txtCidade.Clear();
+            txtTelefone.Clear();
+
         }
 
-
- 
-
-        private void cmdAlterar_Click(object sender, EventArgs e)
+        private void FrmAlterar_Load(object sender, EventArgs e)
         {
-            
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "SERVER = LAPTOP-PG3TNAT4\\SQLEXPRESS;Database=Agenda;UID=sa;PWD=123;";
+            cn.Open();
+
+            SqlDataAdapter da = new SqlDataAdapter("Select *from DadosAgenda", cn);
+
+            DataSet ds = new DataSet();
+
+            da.Fill(ds);
+
+            comboBox1.DisplayMember = "Nome";      //mostrar os nomes para o usuario
+            comboBox1.ValueMember = "Codigo";      //guardar o codigo do nome para dps alterar no banco de dados
+            comboBox1.DataSource = ds.Tables[0];   //combo box recebe as tabelas criada
+            cn.Close();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int codigoAgenda = int.Parse(comboBox1.SelectedValue.ToString());
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "SERVER = LAPTOP-PG3TNAT4\\SQLEXPRESS;Database=Agenda;UID=sa;PWD=123;";
+            SqlCommand cd = new SqlCommand();
+            cd.CommandText = $"Select * from DadosAgenda where Codigo = {codigoAgenda}";
+            cd.Connection = cn;
+            cn.Open();
+
+            SqlDataReader dr = cd.ExecuteReader();
+
+            if (dr.Read())
+            {
+                txtTelefone.Text = dr["Telefone"].ToString();
+                txtCidade.Text = dr["Cidade"].ToString();
+            }
+            cn.Close();
         }
     }
 }
