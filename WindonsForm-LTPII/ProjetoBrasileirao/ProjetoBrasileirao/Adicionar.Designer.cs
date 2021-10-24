0660d023a1439de54a1c47e94415c10e38e2dfcd
GustavoLuizGit <glogustavo4321@gmail.com>
2021-10-24 11:38:01 -0300

    Arrumando  criações de click que nao foi eu que criei ...

--- WindonsForm-LTPII/ProjetoBrasileirao/ProjetoBrasileirao/Adicionar.Designer.cs
+++ WindonsForm-LTPII/ProjetoBrasileirao/ProjetoBrasileirao/Adicionar.Designer.cs
@@ -52,12 +52,11 @@ namespace ProjetoBrasileirao
             this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label1.Font = new System.Drawing.Font("Showcard Gothic", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(220, 0);
+            this.label1.Location = new System.Drawing.Point(221, -3);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(497, 60);
             this.label1.TabIndex = 1;
             this.label1.Text = "Adicione seu time ";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
@@ -65,12 +64,11 @@ namespace ProjetoBrasileirao
             this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(118, 120);
+            this.label2.Location = new System.Drawing.Point(119, 117);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(244, 37);
             this.label2.TabIndex = 2;
             this.label2.Text = "Nome do time : ";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
@@ -78,12 +76,11 @@ namespace ProjetoBrasileirao
             this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label3.Location = new System.Drawing.Point(82, 174);
+            this.label3.Location = new System.Drawing.Point(83, 171);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(280, 37);
             this.label3.TabIndex = 3;
             this.label3.Text = "Data de fundação :";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
@@ -91,12 +88,11 @@ namespace ProjetoBrasileirao
             this.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label4.Location = new System.Drawing.Point(23, 228);
+            this.label4.Location = new System.Drawing.Point(24, 225);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(339, 37);
             this.label4.TabIndex = 4;
             this.label4.Text = "Quantidade de titulos :";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label5
             // 
@@ -104,19 +100,18 @@ namespace ProjetoBrasileirao
             this.label5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label5.Location = new System.Drawing.Point(96, 283);
+            this.label5.Location = new System.Drawing.Point(97, 280);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(266, 37);
             this.label5.TabIndex = 5;
             this.label5.Text = "Imagem do time :";
-            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // cmdInserirImagem
             // 
             this.cmdInserirImagem.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.cmdInserirImagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmdInserirImagem.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.cmdInserirImagem.Location = new System.Drawing.Point(448, 327);
+            this.cmdInserirImagem.Location = new System.Drawing.Point(449, 324);
             this.cmdInserirImagem.Name = "cmdInserirImagem";
             this.cmdInserirImagem.Size = new System.Drawing.Size(126, 42);
             this.cmdInserirImagem.TabIndex = 7;
@@ -133,7 +128,7 @@ namespace ProjetoBrasileirao
             this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(313, 489);
+            this.button1.Location = new System.Drawing.Point(314, 486);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(314, 79);
             this.button1.TabIndex = 8;
@@ -146,22 +141,20 @@ namespace ProjetoBrasileirao
             this.txtTitulos.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.txtTitulos.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtTitulos.ForeColor = System.Drawing.SystemColors.Info;
-            this.txtTitulos.Location = new System.Drawing.Point(415, 228);
+            this.txtTitulos.Location = new System.Drawing.Point(416, 225);
             this.txtTitulos.Name = "txtTitulos";
             this.txtTitulos.Size = new System.Drawing.Size(204, 34);
             this.txtTitulos.TabIndex = 9;
-            this.txtTitulos.TextChanged += new System.EventHandler(this.txtTitulos_TextChanged);
             // 
             // txtFundacao
             // 
             this.txtFundacao.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.txtFundacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.txtFundacao.ForeColor = System.Drawing.SystemColors.Info;
-            this.txtFundacao.Location = new System.Drawing.Point(415, 177);
+            this.txtFundacao.Location = new System.Drawing.Point(416, 174);
             this.txtFundacao.Name = "txtFundacao";
             this.txtFundacao.Size = new System.Drawing.Size(204, 34);
             this.txtFundacao.TabIndex = 10;
-            this.txtFundacao.TextChanged += new System.EventHandler(this.txtFundacao_TextChanged);
             // 
             // txtNome
             // 
@@ -176,13 +169,12 @@ namespace ProjetoBrasileirao
             // pictureBox2
             // 
             this.pictureBox2.Image = global::ProjetoBrasileirao.Properties.Resources.x;
-            this.pictureBox2.Location = new System.Drawing.Point(415, 283);
+            this.pictureBox2.Location = new System.Drawing.Point(416, 280);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(204, 127);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 6;
             this.pictureBox2.TabStop = false;
-            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // pictureBox1
             // 
@@ -213,7 +205,6 @@ namespace ProjetoBrasileirao
             this.Controls.Add(this.pictureBox1);
             this.Name = "Adicionar";
             this.Text = "Adicionar";
-            this.Load += new System.EventHandler(this.Adicionar_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
--- WindonsForm-LTPII/ProjetoBrasileirao/ProjetoBrasileirao/Adicionar.cs
+++ WindonsForm-LTPII/ProjetoBrasileirao/ProjetoBrasileirao/Adicionar.cs
@@ -30,52 +30,7 @@ namespace ProjetoBrasileirao
             }
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label5_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void pictureBox2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Adicionar_Load(object sender, EventArgs e)
-        {
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtTitulos_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void txtFundacao_TextChanged(object sender, EventArgs e)
         {
 
         }
