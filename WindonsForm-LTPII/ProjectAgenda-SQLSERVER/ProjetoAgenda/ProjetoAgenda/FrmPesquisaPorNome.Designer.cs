b3bcf92f61f24858d060a71ceaea0479e18d0d67
GustavoLuizGit <glogustavo4321@gmail.com>
2021-10-24 11:16:34 -0300

    Pesquisa Por Nome e Cidade
    Fazendo Pesquisa no forms atraves do SQL

---
+++ WindonsForm-LTPII/ProjectAgenda-SQLSERVER/ProjetoAgenda/ProjetoAgenda/FrmPesquisaPorNome.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace ProjetoAgenda
+{
+    partial class FrmPesquisaPorNome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmdPesquisar = new System.Windows.Forms.Button();
+            this.dgvMostra = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMostra)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(109, 28);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(248, 29);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Pesquisa pelo Nome";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(141, 88);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(286, 30);
+            this.txtNome.TabIndex = 9;
+            this.txtNome.TextChanged += new System.EventHandler(this.txtNome_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 24);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Nome :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // cmdPesquisar
+            // 
+            this.cmdPesquisar.Location = new System.Drawing.Point(13, 145);
+            this.cmdPesquisar.Name = "cmdPesquisar";
+            this.cmdPesquisar.Size = new System.Drawing.Size(414, 47);
+            this.cmdPesquisar.TabIndex = 11;
+            this.cmdPesquisar.Text = "Pesquisar";
+            this.cmdPesquisar.UseVisualStyleBackColor = true;
+            this.cmdPesquisar.Click += new System.EventHandler(this.cmdPesquisar_Click);
+            // 
+            // dgvMostra
+            // 
+            this.dgvMostra.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMostra.Location = new System.Drawing.Point(11, 235);
+            this.dgvMostra.Name = "dgvMostra";
+            this.dgvMostra.RowHeadersWidth = 51;
+            this.dgvMostra.RowTemplate.Height = 24;
+            this.dgvMostra.Size = new System.Drawing.Size(430, 191);
+            this.dgvMostra.TabIndex = 12;
+            this.dgvMostra.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMostra_CellContentClick);
+            // 
+            // FrmPesquisaPorNome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(480, 450);
+            this.Controls.Add(this.dgvMostra);
+            this.Controls.Add(this.cmdPesquisar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmPesquisaPorNome";
+            this.Text = "FrmPesquisa";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMostra)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button cmdPesquisar;
+        private System.Windows.Forms.DataGridView dgvMostra;
+    }
+}
---
+++ WindonsForm-LTPII/ProjectAgenda-SQLSERVER/ProjetoAgenda/ProjetoAgenda/FrmPesquisaPorNome.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ProjetoAgenda
+{
+    public partial class FrmPesquisaPorNome : Form
+    {
+        public FrmPesquisaPorNome()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdPesquisar_Click(object sender, EventArgs e)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "SERVER = LAPTOP-PG3TNAT4\\SQLEXPRESS;Database=Agenda;UID=sa;PWD=123;";
+            cn.Open();
+
+            string sql = $"Select * from DadosAgenda where Nome LIKE '{txtNome.Text}%' ";
+            SqlDataAdapter da = new SqlDataAdapter(sql,cn);
+
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            dgvMostra.DataSource = ds.Tables[0];
+        }
+
+        private void dgvMostra_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtNome_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
