a3bb0acae2a0c3f39931601e2d5dafd6326b0af7
GustavoLuizGit <glogustavo4321@gmail.com>
2021-10-25 11:53:52 -0300

    Partes necessarias para o programa rodar

--- WindonsForm-LTPII/ProjetoBrasileirao/ProjetoBrasileirao/Excluir.Designer.cs
+++ WindonsForm-LTPII/ProjetoBrasileirao/ProjetoBrasileirao/Excluir.Designer.cs
@@ -47,6 +47,7 @@ namespace ProjetoBrasileirao
             // comboBox1
             // 
             this.comboBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.ForeColor = System.Drawing.SystemColors.InactiveBorder;
             this.comboBox1.FormattingEnabled = true;
