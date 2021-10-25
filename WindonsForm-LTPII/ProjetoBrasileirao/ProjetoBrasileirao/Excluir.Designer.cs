dd49774a6192aa40492e9a9af146c30a4a991632
GustavoLuizGit <glogustavo4321@gmail.com>
2021-10-25 11:30:10 -0300

    Excluir dados do SQL atraves do forms
    Exclusão dos dados do SQL, fiz função para carregar o combo e atualizar de uma vez no combo box do forms

---
+++ WindonsForm-LTPII/ProjetoBrasileirao/ProjetoBrasileirao/Excluir.Designer.cs
@@ -0,0 +1,229 @@
+﻿
+namespace ProjetoBrasileirao
+{
+    partial class Excluir
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cmdExcluir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTitulos = new System.Windows.Forms.TextBox();
+            this.txtFundacao = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cmdFechar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtEpico = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(447, 142);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(204, 37);
+            this.comboBox1.TabIndex = 31;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // cmdExcluir
+            // 
+            this.cmdExcluir.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.cmdExcluir.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdExcluir.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.cmdExcluir.Location = new System.Drawing.Point(325, 366);
+            this.cmdExcluir.Name = "cmdExcluir";
+            this.cmdExcluir.Size = new System.Drawing.Size(208, 46);
+            this.cmdExcluir.TabIndex = 30;
+            this.cmdExcluir.Text = "Excluir";
+            this.cmdExcluir.UseVisualStyleBackColor = false;
+            this.cmdExcluir.Click += new System.EventHandler(this.cmdExcluir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(288, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 49);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "Excluir Time";
+            // 
+            // txtTitulos
+            // 
+            this.txtTitulos.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtTitulos.Enabled = false;
+            this.txtTitulos.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTitulos.ForeColor = System.Drawing.SystemColors.Info;
+            this.txtTitulos.Location = new System.Drawing.Point(447, 253);
+            this.txtTitulos.Name = "txtTitulos";
+            this.txtTitulos.Size = new System.Drawing.Size(204, 34);
+            this.txtTitulos.TabIndex = 28;
+            // 
+            // txtFundacao
+            // 
+            this.txtFundacao.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtFundacao.Enabled = false;
+            this.txtFundacao.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFundacao.ForeColor = System.Drawing.SystemColors.Info;
+            this.txtFundacao.Location = new System.Drawing.Point(447, 199);
+            this.txtFundacao.Name = "txtFundacao";
+            this.txtFundacao.Size = new System.Drawing.Size(204, 34);
+            this.txtFundacao.TabIndex = 27;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Black;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(55, 250);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(339, 37);
+            this.label4.TabIndex = 26;
+            this.label4.Text = "Quantidade de titulos :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label3.Location = new System.Drawing.Point(114, 196);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(280, 37);
+            this.label3.TabIndex = 25;
+            this.label3.Text = "Data de fundação :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(150, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(244, 37);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Nome do time : ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProjetoBrasileirao.Properties.Resources.mh_interna_istock_992892836;
+            this.pictureBox1.Location = new System.Drawing.Point(-7, -4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(837, 513);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 23;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cmdFechar
+            // 
+            this.cmdFechar.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.cmdFechar.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdFechar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.cmdFechar.Location = new System.Drawing.Point(357, 435);
+            this.cmdFechar.Name = "cmdFechar";
+            this.cmdFechar.Size = new System.Drawing.Size(158, 46);
+            this.cmdFechar.TabIndex = 32;
+            this.cmdFechar.Text = "Fechar ";
+            this.cmdFechar.UseVisualStyleBackColor = false;
+            this.cmdFechar.Click += new System.EventHandler(this.cmdFechar_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Black;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(144, 302);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(250, 37);
+            this.label5.TabIndex = 33;
+            this.label5.Text = "Titulo Historico :";
+            // 
+            // txtEpico
+            // 
+            this.txtEpico.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtEpico.Enabled = false;
+            this.txtEpico.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEpico.ForeColor = System.Drawing.SystemColors.Info;
+            this.txtEpico.Location = new System.Drawing.Point(447, 305);
+            this.txtEpico.Name = "txtEpico";
+            this.txtEpico.Size = new System.Drawing.Size(204, 34);
+            this.txtEpico.TabIndex = 34;
+            // 
+            // Excluir
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(828, 493);
+            this.Controls.Add(this.txtEpico);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cmdFechar);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cmdExcluir);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTitulos);
+            this.Controls.Add(this.txtFundacao);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Excluir";
+            this.Text = "Excluir";
+            this.Load += new System.EventHandler(this.Excluir_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button cmdExcluir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtTitulos;
+        private System.Windows.Forms.TextBox txtFundacao;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button cmdFechar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtEpico;
+    }
+}
---
+++ WindonsForm-LTPII/ProjetoBrasileirao/ProjetoBrasileirao/Excluir.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ProjetoBrasileirao
+{
+    public partial class Excluir : Form
+    {
+        public Excluir()
+        {
+            InitializeComponent();
+        }
+
+        private int idTimeaux;
+        void carregaTimes()
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "SERVER = LAPTOP-PG3TNAT4\\SQLEXPRESS;Database=Brasileirao;UID=sa;PWD=123;";
+            cn.Open();
+            SqlDataAdapter da = new SqlDataAdapter("Select * from Times", cn);   // ja passa pelo construtor o comando select
+
+            DataSet ds = new DataSet();
+
+            da.Fill(ds);  //preenche o banco de dados virtual(ds) com o comando feito pelo da;
+
+            comboBox1.DisplayMember = "nome";
+            comboBox1.ValueMember = "idTime";
+            comboBox1.DataSource = ds.Tables[0];      // preenche o combobox com a tabela, no entanto a gente pede pra so mostrar on ome
+            cn.Close();
+        }
+        private void Excluir_Load(object sender, EventArgs e)
+        {
+            carregaTimes();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            idTimeaux = int.Parse(comboBox1.SelectedValue.ToString());
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "SERVER = LAPTOP-PG3TNAT4\\SQLEXPRESS;Database=Brasileirao;UID=sa;PWD=123;";
+            cn.Open();
+            SqlCommand cd = new SqlCommand();
+            cd.CommandText = "Select * from Times where idTime = " + idTimeaux.ToString();
+            cd.Connection = cn;
+
+            SqlDataReader dr = cd.ExecuteReader();  //dr = a pesquisa feita pelo select 
+
+            if (dr.Read()) //se tiver algo na tabela lida o if vai ser verdadeiro
+            {
+                txtTitulos.Text = dr["quantidadeTitulos"].ToString();
+                txtFundacao.Text = dr["fundacao"].ToString();
+                txtEpico.Text = dr["tituloEpico"].ToString();
+            }
+            cn.Close();
+
+        }
+
+        private void cmdExcluir_Click(object sender, EventArgs e)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "SERVER = LAPTOP-PG3TNAT4\\SQLEXPRESS;Database=Brasileirao;UID=sa;PWD=123;";
+            cn.Open();
+
+            SqlCommand cd = new SqlCommand();
+            cd.CommandText = $"Delete Times where idTime = {idTimeaux.ToString()}";
+            cd.Connection = cn;
+            cd.ExecuteNonQuery();
+            MessageBox.Show("Time excluido com suucesso!!");
+            carregaTimes();
+            cn.Close();
+        }
+
+        private void cmdFechar_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
