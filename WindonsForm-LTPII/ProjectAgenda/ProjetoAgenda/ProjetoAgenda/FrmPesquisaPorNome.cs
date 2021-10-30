0d649a53520ca4b6294ba09ea5d5e20edce63fad
GustavoLuizGit <glogustavo4321@gmail.com>
2021-10-30 14:14:43 -0300

    PesquisaPorCidadeEnome-TelaPrincipal
    Codificação bruta sem utilizar classes

---
+++ WindonsForm-LTPII/ProjectAgenda/ProjetoAgenda/ProjetoAgenda/FrmPesquisaPorNome.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ProjetoAgenda
+{
+    public partial class FrmPesquisaPorNome : Form
+    {
+        public FrmPesquisaPorNome()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdPesquisar_Click(object sender, EventArgs e)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = "SERVER = LAPTOP-PG3TNAT4\\SQLEXPRESS;Database=Agenda;UID=sa;PWD=123;";
+            cn.Open();
+
+            string sql = $"Select * from DadosAgenda where Nome LIKE '{txtNome.Text}%' ";
+            SqlDataAdapter da = new SqlDataAdapter(sql, cn);
+
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+
+            dgvMostra.DataSource = ds.Tables[0];
+        }
+    }
+}
