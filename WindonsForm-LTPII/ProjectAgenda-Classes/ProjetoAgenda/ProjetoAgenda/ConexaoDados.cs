016393180c0283d7b756b6ae156e427bb8787ac7
GustavoLuizGit <glogustavo4321@gmail.com>
2021-10-30 11:51:46 -0300

    Projeto agenda utilizando classes
    Classes DadosAgenda e conexao de dados

---
+++ WindonsForm-LTPII/ProjectAgenda-Classes/ProjetoAgenda/ProjetoAgenda/ConexaoDados.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ProjetoAgenda
+{
+    class ConexaoDados
+    {
+        SqlConnection cn = new SqlConnection();
+        SqlCommand cd = new SqlCommand();
+        public SqlDataReader dr;
+        SqlDataAdapter da;
+        public DataSet ds;
+        public void Conectar()
+        {
+            cn.ConnectionString = "SERVER = LAPTOP-PG3TNAT4\\SQLEXPRESS;Database=Agenda;UID=sa;PWD=123;";
+            cn.Open();
+        }
+
+        public void Desconectar()
+        {
+            cn.Close();
+        }
+
+        public void Executar(string sql)
+        {
+            Conectar();
+            cd.Connection = cn;
+            cd.CommandText = sql;
+            cd.ExecuteNonQuery();
+            Desconectar();
+        }
+
+        public void Listar(string sql)
+        {
+            Conectar();
+            da = new SqlDataAdapter(sql,cn);
+            ds = new DataSet();
+            da.Fill(ds);
+            Desconectar();
+        }
+
+        public void Consultar(string sql)
+        {
+            Conectar();
+            cd.Connection = cn;
+            cd.CommandText = sql;
+            dr = cd.ExecuteReader();
+        }
+    }
+}
---
+++ WindonsForm-LTPII/ProjectAgenda-Classes/ProjetoAgenda/ProjetoAgenda/DadosAgenda.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ProjetoAgenda
+{
+    class DadosAgenda
+    {
+        private int codigo;
+        private string nome;
+        private string telefone;
+        private string cidade;
+
+        public int Codigo { get => codigo; set => codigo = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public string Telefone { get => telefone; set => telefone = value; }
+        public string Cidade { get => cidade; set => cidade = value; }
+
+        private ConexaoDados objetoConexao = new ConexaoDados();
+        public void Inserir()
+        {
+            string sql = $"Insert into DadosAgenda (Nome, Telefone, Cidade) values ('{Nome}', '{Telefone}', '{Cidade}')";
+            objetoConexao.Executar(sql);
+        }
+
+        public void Alterar()
+        {
+            string sql = "Update DadosAgenda set Telefone = '" + Telefone+ "',Cidade = '" + Cidade+ "'Where Codigo =" + Codigo.ToString();
+            objetoConexao.Executar(sql);
+        }
+
+        public void Excluir()
+        {
+            string sql = $"Delete DadosAgenda where codigo = {Codigo.ToString()}";
+            objetoConexao.Executar(sql);
+        }
+
+        public void ConsultarDados()
+        {
+            string sql = $"Select * from DadosAgenda where Codigo = {Codigo}";
+            objetoConexao.Consultar(sql);
+
+            if (objetoConexao.dr.Read())
+            {
+                Telefone = objetoConexao.dr["Telefone"].ToString();
+                Cidade = objetoConexao.dr["Cidade"].ToString();
+            }
+            objetoConexao.Desconectar();
+        }
+
+        public DataSet ListarDados()
+        {
+            string sql = "Select * from DadosAgenda";
+            objetoConexao.Listar(sql);
+            return objetoConexao.ds;
+        }
+
+        public DataSet ListarDadosPorNome()
+        {
+            string sql = $"Select * from DadosAgenda where Nome LIKE '{Nome}%' ";
+            objetoConexao.Listar(sql);
+            return objetoConexao.ds;
+        }
+
+        public DataSet ListarDadosPorCidade()
+        {
+            string sql = $"Select * from DadosAgenda where Cidade LIKE '{Cidade}%' ";
+            objetoConexao.Listar(sql);
+            return objetoConexao.ds;
+        }
+
+
+    }
+}
